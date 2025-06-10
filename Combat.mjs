45cb74b6bd6ef3d1e925f02d99b781c1896140f9
CARLOS <carlos012100@gmail.com>
2025-06-10 08:58:23 +0200

    [Add]Attributes to all classes, left methods empty for future completion, but basic structure is in place

---
+++ Combat.mjs
@@ -0,0 +1,13 @@
+export default class Combat{
+
+    constructor (fighters, dies){
+
+        this.fighters = fighters;
+        this.dies = dies;
+
+
+    }
+
+
+
+}
---
+++ Die.mjs
@@ -0,0 +1,7 @@
+export default class Die {
+
+    constructor(values){
+
+        this.values = values;
+    }
+}
